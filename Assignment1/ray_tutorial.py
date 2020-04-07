8ae3729eeb69f5a1b54f7dae3e61dc0384fd1d97
divyabajaj0025 <divyabajaj0025@gmail.com>
2020-04-06 17:57:54 -0700

    Changes in Ray 6

--- Assignment1/ray_tutorial.py
+++ Assignment1/ray_tutorial.py
@@ -142,7 +142,7 @@ print('The results are {}. This took {} seconds. Run the next cell to see '
 
 time.sleep(10.0)
 start_time = time.time()
-results1 = ray.get(results)
+results1 = ray.get([slow_function.remote(i) for i in range(4)])
 end_time = time.time()
 duration = end_time - start_time
 
